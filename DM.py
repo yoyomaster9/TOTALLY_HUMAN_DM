fd3901d578da3b0c3ecc282a473544bae672a1e3
yoyomaster9 <yoyomaster9@yahoo.com>
2019-05-24 13:49:48 -0700

    Added functionality for modifiers

--- DM.py
+++ DM.py
@@ -4,14 +4,17 @@ def roll(s): # Simulates rolls of the form #d#+#d#..
     l = []
     s = s.lower()
     for i in s.split('+'):
-        try:
-            if i[0] == 'd':
+        if 'd' in i:
+            try:
+                if i[0] == 'd':
+                    i = '1' + i
+            except:
                 i = '1' + i
-        except:
-            i = '1' + i
-        [n, max] = [int(x) for x in i.split('d')]
-        for j in range(n):
-            l.append(random.randint(1, max))
+            [n, max] = [int(x) for x in i.split('d')]
+            for j in range(n):
+                l.append(random.randint(1, max))
+        else:
+            l.append(int(i))
     return l
 
 #
